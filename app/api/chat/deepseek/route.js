1e81d21ca9495a1a5278e09c26b55d8e2ac301da
MahfujulSagor <me24020@mbstu.ac.bd>
2025-04-02 18:48:25 +0600

    add encryption and decryption

--- app/api/chat/deepseek/route.js
+++ app/api/chat/deepseek/route.js
@@ -1,41 +1,8 @@
 import { databases } from "@/lib/appwrite";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import crypto from "crypto";
 import { Query } from "appwrite";
-
-// AES encryption setup (used for model name)
-const secretKey = Buffer.from(process.env.SECRET_KEY, "hex"); // Convert hex string to Buffer
-const ivLength = 16; // IV length for AES-256-CBC (16 bytes)
-
-if (!secretKey) {
-  throw new Error("Secret key is not set in environment variables.");
-}
-
-// Encrypt function
-function encrypt(text) {
-  const iv = crypto.randomBytes(ivLength); // New IV for each encryption
-  const cipher = crypto.createCipheriv(
-    "aes-256-cbc",
-    Buffer.from(secretKey, "hex"),
-    iv
-  );
-  let encrypted = cipher.update(text, "utf8", "hex");
-  encrypted += cipher.final("hex");
-  return { encrypted, iv: iv.toString("hex") };
-}
-
-// Decrypt function
-function decrypt(encryptedText, iv) {
-  const decipher = crypto.createDecipheriv(
-    "aes-256-cbc",
-    Buffer.from(secretKey, "hex"),
-    Buffer.from(iv, "hex")
-  );
-  let decrypted = decipher.update(encryptedText, "hex", "utf8");
-  decrypted += decipher.final("utf8");
-  return decrypted;
-}
+import { decrypt, encrypt } from "@/lib/encrypt_decrypt";
 
 export const POST = async (req) => {
   const { prompt, model_id } = await req.json();
---
+++ app/api/chat/gemini/route.js
@@ -0,0 +1,3 @@
+export const POST = async (req) => {
+  const { prompt, model_id } = await req.json();
+};
