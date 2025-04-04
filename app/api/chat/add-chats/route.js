4fe6ef10e407d1676555c22ce0d2ad8022278e9e
MahfujulSagor <me24020@mbstu.ac.bd>
2025-04-04 20:30:39 +0600

    synce main branch

--- app/api/chat/add-chats/route.js
+++ app/api/chat/add-chats/route.js
@@ -1,6 +1,7 @@
 import { databases } from "@/lib/appwrite";
 import { getUser } from "@/lib/getUser";
 import { ID } from "appwrite";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 const user = await getUser();
@@ -8,8 +9,18 @@ const user = await getUser();
 if (!user) {
   return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
 }
+
 export const POST = async (req) => {
-  const { history } = req.json();
+  const { history } = await req.json();
+  const cookieStore = await cookies();
+  const sessionToken = cookieStore.get("session_token")?.value;
+
+  if (!sessionToken) {
+    return NextResponse.json(
+      { message: "Session token not found" },
+      { status: 401 }
+    );
+  }
 
   try {
     const newChat = await databases.createDocument(
@@ -23,7 +34,7 @@ export const POST = async (req) => {
     );
 
     return NextResponse.json(
-      { newChat },
+      newChat,
       { message: "Chat added to the database." },
       { status: 200 }
     );
