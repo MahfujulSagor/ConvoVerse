b809865480156c08c8f01af9d37cb23a8a629345
MahfujulSagor <me24020@mbstu.ac.bd>
2025-04-22 20:05:02 +0600

    change project name and add metadata

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,19 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
-  title: "Dashboard",
+  title: "ConvoVerse",
   description: "Dashboard",
 };
 
